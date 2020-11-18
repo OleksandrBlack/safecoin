80aae3101ce8e0d87f3cc15a175e9f24cf3a6156
OleksandrBlack <aleksandr.chornuy@gmail.com>
2020-11-19 00:52:38 +0200

    fix

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -28,7 +28,7 @@ endef
 
 
 define $(package)_preprocess_cmds
-  patch -p2 < $($(package)_patch_dir)/signals2-noise.patch
+  patch -p2 < $($(package)_patch_dir)/signals2-noise.patch && \
   echo "using $(boost_toolset_$(host_os)) : : $($(package)_cxx) : <cxxflags>\"$($(package)_cxxflags) $($(package)_cppflags)\" <linkflags>\"$($(package)_ldflags)\" <archiver>\"$(boost_archiver_$(host_os))\" <striper>\"$(host_STRIP)\"  <ranlib>\"$(host_RANLIB)\" <rc>\"$(host_WINDRES)\" : ;" > user-config.jam
 endef
 
